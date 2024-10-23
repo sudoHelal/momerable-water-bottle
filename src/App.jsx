02e1a686f5a4c3e870be300182bc4807e21e21b2
sudohelal <helaluddin40@outlook.com>
2024-10-23 11:01:57 +0600

    display water bottle with React

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Bottles from './components/Bottles/Bottles'
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <Header></Header>
+      <Bottles></Bottles>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Bottles/Bottles.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Bottle from '../Bottle/Bottle';
+import './Bottles.css';
+
+const Bottles = () => {
+  const  [bottles, setBottles] = useState([])
+    useEffect(() =>{
+        fetch('bottles.json')
+        .then(res => res.json())
+        .then(data =>setBottles(data))
+    },[])
+    return (
+        <div>
+            <h2>Bottles here:{bottles.length} </h2>
+            <div className="bottle-container">
+            {
+                bottles.map(bottle => <Bottle
+                key={bottle.id}
+                bottle={bottle}
+                ></Bottle>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Bottles;
