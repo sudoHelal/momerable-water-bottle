474cfc6284ea559f5115e78d8565da51470b7525
sudohelal <helaluddin40@outlook.com>
2024-10-25 15:24:04 +0600

    add and remove from shopping cart

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Bottle from '../Bottle/Bottle';
 import './Bottles.css';
-import { addToLS, getStoredCart } from '../../utilities/localStorage';
+import { addToLS, getStoredCart, removeFromLS } from '../../utilities/localStorage';
 import Cart from '../Cart/Cart';
 
 const Bottles = () => {
@@ -37,10 +37,20 @@ const Bottles = () => {
         setCart(newCart); 
         addToLS(bottle.id);
     }
+
+const handleRemoveFromCart = id =>{
+    // visual cart remove 
+    const remainingCart = cart.filter(bottle => bottle.id !== id);
+    setCart(remainingCart)
+    // remove from LS
+    removeFromLS(id);
+} 
+
+
     return (
         <div>
             <h2>Bottles available:{bottles.length} </h2>
-            <Cart cart={cart}></Cart>
+            <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
             <div className="bottle-container">
             {
                 bottles.map(bottle => <Bottle
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
 import './Cart.css';
-const Cart = ({cart}) => {
+const Cart = ({cart, handleRemoveFromCart}) => {
     return (
         <div>
             <h4>cart: {cart.length}</h4>
             <div className="cart-container">
-                {cart.map(bottle => <img key={bottle.id} src={bottle.img}></img>)}
+                {cart.map(bottle => <div key={bottle.id}>
+                    <img  src={bottle.img}></img>
+                    <button onClick={() => handleRemoveFromCart(bottle.id)}> Remove</button>
+                </div>)}
             </div>
         </div>
     );
 };
 Cart.propTypes = {
-    cart: PropTypes.array.isRequired
+    cart: PropTypes.array.isRequired,
+    handleRemoveFromCart: PropTypes.func.isRequired
 }
 export default Cart;
